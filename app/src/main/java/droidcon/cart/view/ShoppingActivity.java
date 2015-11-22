f076af01c6d6f27f2cdf9c5a9c35301d428c6e38
NazneenRupawalla <nazneenr@thoughtworks.com>
2015-11-22 13:57:53 +0530

    Adding tabbed look to the view pager. Minor UI changes.

--- app/src/main/java/droidcon/cart/view/ShoppingActivity.java
+++ app/src/main/java/droidcon/cart/view/ShoppingActivity.java
@@ -2,23 +2,50 @@ package droidcon.cart.view;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
 import droidcon.cart.R;
 
-public class ShoppingActivity extends FragmentActivity {
+public class ShoppingActivity extends AppCompatActivity {
+
+  private ViewPager viewPager;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.shopping);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+    tabLayout.addTab(tabLayout.newTab().setText(R.string.electronics));
+    tabLayout.addTab(tabLayout.newTab().setText(R.string.accessories));
+    tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-    ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
-    viewPager.setAdapter(getCategoryAdapter(R.string.electronics, R.string.accessories));
+    viewPager = (ViewPager) findViewById(R.id.viewPager);
+    viewPager.setAdapter(getCategoryAdapter(tabLayout.getTabCount()));
+    viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+      @Override
+      public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+      }
+
+      @Override
+      public void onTabUnselected(TabLayout.Tab tab) {
+
+      }
+
+      @Override
+      public void onTabReselected(TabLayout.Tab tab) {
+
+      }
+    });
   }
 
   @Override
@@ -28,7 +55,7 @@ public class ShoppingActivity extends FragmentActivity {
   }
 
   @NonNull
-  private FragmentPagerAdapter getCategoryAdapter(final int... categories) {
+  private FragmentPagerAdapter getCategoryAdapter(final int count) {
     return new FragmentPagerAdapter(getSupportFragmentManager()) {
       @Override
       public Fragment getItem(int position) {
@@ -37,12 +64,7 @@ public class ShoppingActivity extends FragmentActivity {
 
       @Override
       public int getCount() {
-        return categories.length;
-      }
-
-      @Override
-      public CharSequence getPageTitle(int position) {
-        return getString(categories[position]);
+        return count;
       }
     };
   }
