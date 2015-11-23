136fc9ea6618f37d7b90ae96df6f850e2e80ef78
NazneenRupawalla <nazneenr@thoughtworks.com>
2015-11-23 17:48:19 +0530

    Showing num of items in the cart on the action bar.

--- app/src/main/java/droidcon/cart/view/ShoppingActivity.java
+++ app/src/main/java/droidcon/cart/view/ShoppingActivity.java
@@ -5,16 +5,23 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import droidcon.cart.R;
 
 public class ShoppingActivity extends AppCompatActivity {
 
   private ViewPager viewPager;
+  private Button cart;
+  private int numOfItems = 0;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -50,8 +57,20 @@ public class ShoppingActivity extends AppCompatActivity {
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.cart_menu, menu);
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.cart_menu, menu);
+
+    final MenuItem item = menu.findItem(R.id.cart);
+    MenuItemCompat.setActionView(item, R.layout.cart_update_count);
+    View count = item.getActionView();
+    cart = (Button) count.findViewById(R.id.num_of_items);
+    cart.setText(String.valueOf(numOfItems));
     return super.onCreateOptionsMenu(menu);
+  }
+
+  private void setNumOfItems(int count){
+    numOfItems = count;
+    invalidateOptionsMenu();
   }
 
   @NonNull
