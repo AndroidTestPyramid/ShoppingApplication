cb91e0502e591471ae2fd8766f0f9da99ef929f3
NazneenRupawalla <nazneenr@thoughtworks.com>
2015-12-06 02:37:01 +0530

    Changing url to fetch accessories. Also adding id to certain views.

--- app/src/main/java/droidcon/shopping/service/AccessoriesFetcher.java
+++ app/src/main/java/droidcon/shopping/service/AccessoriesFetcher.java
@@ -4,7 +4,7 @@ import droidcon.service.APIClient;
 
 public class AccessoriesFetcher extends ProductsFetcherService {
 
-  public static final String ACCESSORIES_URL = "http://xplorationstudio.com/sample_images/products.json";
+  public static final String ACCESSORIES_URL = "http://xplorationstudio.com/sample_images/accessories.json";
 
   public AccessoriesFetcher(APIClient apiClient){
     super(ACCESSORIES_URL, apiClient);
