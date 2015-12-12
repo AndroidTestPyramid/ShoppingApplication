91e5e58e4d82bfd02c5dd33f2ec2574536768f58
Vidhi Thakrar <vidhitha@thoughtworks.com>
2015-12-12 23:37:22 +0530

    Removed unnecessary imports.

--- app/src/main/java/droidcon/login/view/LoginActivity.java
+++ app/src/main/java/droidcon/login/view/LoginActivity.java
@@ -4,11 +4,8 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import droidcon.cart.R;
